c1a30ed553876c21ec1a24b331f6c7b0780afdfc
alperakaydin <43079310+alperakaydin@users.noreply.github.com>
2024-12-05 14:28:37 +0300

    HomeWork1

---
+++ HomeWork1.kt
@@ -0,0 +1,98 @@
+package com.example.firstapp
+
+fun main() {
+
+    val ilce = "Maltepe"
+    val postaKodu = "34000"
+    val mahalleAdi = "Cevizli Mahallesi"
+    val faks = "012345555555"
+    val instaAdresi = "@alper_akaydin"
+    val telefonModeli = "Xaomi 11Lite 5G"
+    val calistiginBolum = "Mobil Uygulama Geliştirme"
+    val sirket = "FMSS Bilişim"
+    val urunMiktari = 235
+    val satilanMiktar = 181
+    val urunFiyati = 1550.75
+    val odemeMiktari = 2713.5
+    val eftMiktari = 2000.0
+    val odemeSaati = "12:34"
+    val odemeGunu = "2024-12-04"
+    val faturaTarihi = "2024-12-01"
+    val musteriSoyadi = "Akaydın"
+    val dogumTarihi = "1996-01-01"
+    val medeniHal = "Bekar"
+    val borc = 150000.0
+    val dergiAdi = "Bilim Teknik"
+    val basimTarihi = "2024-11-30"
+    val videoAdi = "Kotlin Android"
+    val videoYorumu = "Çok faydalı bir içerik."
+    val muzikSuresi = "03:45"
+    val dosyaAdi = "rapor.pdf"
+    val resimFormati = "JPEG"
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val kullanilanDakika = 120
+    val mekanPuani = 4.5
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val bilgisayarModeli = "MacBook Pro M1"
+    val ekranBoyutu = 13.3
+    val kullanimSuresi = "2 yıl"
+    val etkinlikGunu = "2024-12-10"
+    val yolculukCikisTarihi = "2024-12-15"
+    val otobusHatti = "500T"
+    val kargoTakipNo = "ABC123456789"
+    val kuponSuresi = "2024-12-31"
+    val kuponKodu = "YILBASI2024"
+    val zamMiktari = 500.0
+    val yemekAdi = "Kebap"
+    val daireSayisi = 10
+    val basinc = 1013.25
+
+    // Çıktılar
+    println("Posta Kodu: $postaKodu")
+    println("Mahalle Adı: $mahalleAdi")
+    println("Faks: $faks")
+    println("Instagram Adresi: $instaAdresi")
+    println("Telefon Modeli: $telefonModeli")
+    println("Çalıştığın Bölüm: $calistiginBolum")
+    println("Şirket: $sirket")
+    println("Ürün Miktarı: $urunMiktari")
+    println("Satılan Miktar: $satilanMiktar")
+    println("Ürün Fiyatı: $urunFiyati")
+    println("Ödeme Miktarı: $odemeMiktari")
+    println("EFT Miktarı: $eftMiktari")
+    println("Ödeme Saati: $odemeSaati")
+    println("Ödeme Günü: $odemeGunu")
+    println("Fatura Tarihi: $faturaTarihi")
+    println("Müşteri Soyadı: $musteriSoyadi")
+    println("Doğum Tarihi: $dogumTarihi")
+    println("Medeni Hal: $medeniHal")
+    println("Borç: $borc")
+    println("Dergi Adı: $dergiAdi")
+    println("Basım Tarihi: $basimTarihi")
+    println("Video Adı: $videoAdi")
+    println("Video Yorumu: $videoYorumu")
+    println("Müzik Süresi: $muzikSuresi")
+    println("Dosya Adı: $dosyaAdi")
+    println("Resim Formatı: $resimFormati")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Kullanılan Dakika: $kullanilanDakika")
+    println("Mekan Puanı: $mekanPuani")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Bilgisayar Modeli: $bilgisayarModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Kullanım Süresi: $kullanimSuresi")
+    println("Etkinlik Günü: $etkinlikGunu")
+    println("Yolculuk Çıkış Tarihi: $yolculukCikisTarihi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kargo Takip No: $kargoTakipNo")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Zam Miktarı: $zamMiktari")
+    println("Yemek Adı: $yemekAdi")
+    println("Daire Sayısı: $daireSayisi")
+    println("Basınç: $basinc")
+}
